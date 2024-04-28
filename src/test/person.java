a25f200d631e7bbfea983fe59744afaf8cdba661
ssoz <ksy001028@naver,com>
2024-04-28 18:50:20 +0900

    first commit

---
+++ src/test/person.java
@@ -0,0 +1,46 @@
+package test;
+
+public class person {
+	
+	private String name;
+	private String age;
+	private String[] hobbies;
+	
+	
+
+	public String getName() {
+		return name;
+	}
+
+
+
+	public String getAge() {
+		return age;
+	}
+
+
+
+	public String[] getHobbies() {
+		return hobbies;
+	}
+	
+	public void speak() {
+		System.out.println("HEELO");
+	}
+	public void eat() {
+		System.out.println("욤욤");
+	}
+	public void walk() {
+		System.out.println("산책");
+	}
+	public void sleep() {
+		System.out.println("낸내");
+	}
+
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
